891293bfd09ac2a13b06d686a1ba4c1abc3102c9
eya1528 <djebbieya89@gmail.com>
2025-04-06 16:08:13 +0200

    Update array.js

--- array.js
+++ array.js
@@ -1,10 +1,23 @@
-function findMax(arr) {
-    return Math.max(...arr);
+function findMinAndMax(arr) {
+    if (arr.length === 0) {
+        return null; // or throw an error if you prefer
+    }
+
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return { min, max };
 }
 
-function findMin(arr) {
-    return Math.min(...arr);
-}
 
 function sumArray(arr) {
     return arr.reduce((sum, num) => sum + num, 0);
